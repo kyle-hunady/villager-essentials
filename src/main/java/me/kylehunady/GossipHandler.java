af9f14beab3ed56363f2466254ebe05cb259c397
Kyle Hunady <kyokobra@gmail.com>
2022-07-13 10:17:07 -0700

    fixed issue that would prematurely end writing player data to gossips

--- src/main/java/me/kylehunady/GossipHandler.java
+++ src/main/java/me/kylehunady/GossipHandler.java
@@ -1,20 +1,23 @@
 package me.kylehunady;
 
 import java.util.Arrays;
-import java.util.Random;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.entity.ZombieVillager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
 import io.github.bananapuncher714.nbteditor.NBTEditor;
 import io.github.bananapuncher714.nbteditor.NBTEditor.NBTCompound;
+
+// START DEBUGGING IMPORTS
+// import java.util.Random;
+// import org.bukkit.Bukkit;
+// import org.bukkit.event.entity.EntityDamageByEntityEvent;
+// END DEBUGGING IMPORTS
 
 public class GossipHandler implements Listener {
 
@@ -23,12 +26,10 @@ public class GossipHandler implements Listener {
 
         // check if player is right clicking a villager or zombie villager
         Entity entity = event.getRightClicked();
-        if (!(entity instanceof Villager || entity instanceof ZombieVillager))
-            return;
+        if (!(entity instanceof Villager || entity instanceof ZombieVillager)) return;
 
         // check if the villager has a Gossips property to parse
-        if (!(NBTEditor.contains(entity, "Gossips", 0)))
-            return;
+        if (!(NBTEditor.contains(entity, "Gossips", 0))) return;
 
         // the following native MC command (cheats enabled) will grab the Gossips data
         // of the nearest villager
@@ -47,45 +48,53 @@ public class GossipHandler implements Listener {
 
         // Note: Gossips property is a list. Each element is a compound tag containing
         // Target, Type, and Value
-        NBTCompound gossips = NBTEditor.getNBTCompound(entity, "Gossips");
-        int size = NBTEditor.getSize(gossips);
-
+        
         Player player = event.getPlayer();
-        int[] playerId = NBTEditor.getIntArray(player, "UUID");
-
-        int maxValue = 0;
-        // index is needed to change the player's existing entry in Gossips property
-        int playerIndex = -1;
 
         String[] gossipTypes = { "minor_positive", "major_positive" };
         for (String type : gossipTypes) {
-            for (int i = 0; i < size; i++) {
-                // grab the ith element in Gossips and check its Type
-                if (NBTEditor.getString(gossips, i, "Type").equals(type)) {
-                    int val = NBTEditor.getInt(gossips, i, "Value");
-                    maxValue = (val > maxValue) ? val : maxValue;
+            addPlayerToGossip(player, entity, type);
+        }
+    }
 
-                    // determine if the player already has a Gossips entry and save index
-                    if (Arrays.equals(playerId, NBTEditor.getIntArray(gossips, i, "Target")))
-                        playerIndex = i;
+    private void addPlayerToGossip(Player player, Entity entity, String gossipType) {
+        int maxValue = 0;
+        // index is needed to change the player's existing entry in Gossips property
+        int playerIndex = -1;
+        int playerValue = 0;
+
+        NBTCompound gossips = NBTEditor.getNBTCompound(entity, "Gossips");
+        int size = NBTEditor.getSize(gossips);
+        int[] playerId = NBTEditor.getIntArray(player, "UUID");
+        
+        for (int i = 0; i < size; i++) {
+            // grab the ith element in Gossips and check its Type
+            if (NBTEditor.getString(gossips, i, "Type").equals(gossipType)) {
+                int val = NBTEditor.getInt(gossips, i, "Value");
+                maxValue = (val > maxValue) ? val : maxValue;
+
+                // determine if the player already has a Gossips entry and save index
+                if (Arrays.equals(playerId, NBTEditor.getIntArray(gossips, i, "Target"))) {
+                    playerIndex = i;
+                    playerValue = val;
                 }
-
             }
-            if (maxValue <= 0)
-                return; // if no positive gossip exists, we don't need to update the villager data
 
-            NBTCompound newGossipEntry = NBTEditor.getEmptyNBTCompound();
-            newGossipEntry.set(playerId, "Target");
-            newGossipEntry.set(type, "Type");
-            newGossipEntry.set(maxValue, "Value");
+        }
+        if (maxValue <= 0) return; // if no positive gossip exists, we don't need to update the villager data
+        if (playerValue == maxValue) return; // if player has max value, we don't need to update either
 
+        NBTCompound newGossipEntry = NBTEditor.getEmptyNBTCompound();
+        newGossipEntry.set(playerId, "Target");
+        newGossipEntry.set(gossipType, "Type");
+        newGossipEntry.set(maxValue, "Value");
+
+        if (playerIndex >= 0) {
             // this function can be a little backwards to read
-            // take our villager and save the new entry to Gossips[index]
-            if (playerIndex >= 0) {
-                NBTEditor.set(entity, newGossipEntry, "Gossips", playerIndex);
-            } else {
-                NBTEditor.set(entity, newGossipEntry, "Gossips", NBTEditor.NEW_ELEMENT);
-            }
+            // take our villager data and save the new entry to Gossips[index]
+            NBTEditor.set(entity, newGossipEntry, "Gossips", playerIndex);
+        } else {
+            NBTEditor.set(entity, newGossipEntry, "Gossips", NBTEditor.NEW_ELEMENT);
         }
     }
 
